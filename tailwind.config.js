3ce3becb5fa179773d9e847e366a630a32628a12
Dhruv-mak <dhruv.makwana.169@gmail.com>
2023-05-21 13:44:02 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        bright_orange: "hsl(31, 77%, 52%)",
+        dark_cyan: "hsl(184, 100%, 22%)",
+        very_dark_cyan: "hsl(179, 100%, 13%)",
+        transparent_white: "hsla(0, 0%, 100%, 0.75)",
+        very_light_gray: "hsl(0, 0%, 95%)"
+      }
+    },
+    fontFamily: {
+      shoulder: ['Big Shoulders Display', 'cursive'],
+      lexend: ['Lexend Deca', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
